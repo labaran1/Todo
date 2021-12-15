8ed993550f31a2747fcf89371fd3a713d862f737
labaran1 <adlabaran@gmail.com>
2021-12-15 07:39:20 +0000

    Notfound route

---
+++ src/views/Notfound.vue
@@ -0,0 +1,13 @@
+<template>
+  <center>
+    <h3>....Hello Friend</h3>
+    <h5>you landed on a wrong page</h5>
+    <router-link to="/">Visit Home</router-link>
+  </center>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
